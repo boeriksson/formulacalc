04dbf4cc68903a85a053d2f35afdf7a4221d22d6
boeriksson <bo.sandevind@tele2.com>
2020-12-26 23:08:57 +0100

    wip

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,90 @@ import * as calc from './index'
 
 describe('#default', () => {
     it('datest', () => {
-        expect(calc.default(1, 'a')).toBe(1)
+        expect(calc.default(1, 'a + 2b - 33')).toBe(1)
+    })
+})
+
+describe('#addImplicitMultiply', () => {
+    it('should add multiply to "2y"', () => {
+        const initialFormulaArray = ['2y']
+        const expectedFormulaArray = ['2', '*', 'y']
+        calc.addImplicitMultiply(initialFormulaArray)
+        expect(initialFormulaArray).toEqual(expectedFormulaArray)
+    })
+    it('should add multiply to "2 * y2x"', () => {
+        const initialFormulaArray = ['2', '*', 'y2x']
+        const expectedFormulaArray = ['2', '*', 'y', '*', '2', '*', 'x']
+        calc.addImplicitMultiply(initialFormulaArray)
+        expect(initialFormulaArray).toEqual(expectedFormulaArray)
+    })
+    it('should add multiply to "2 * (y2x)"', () => {
+        const initialFormulaArray = ['2', '*', '(', 'y2x', ')']
+        const expectedFormulaArray = ['2', '*', '(', 'y', '*', '2', '*', 'x', ')']
+        calc.addImplicitMultiply(initialFormulaArray)
+        expect(initialFormulaArray).toEqual(expectedFormulaArray)
+    })
+})
+
+describe('#splitElement', () => {
+    it('should not split digits', () => {
+        expect(calc.splitElement('1234')).toStrictEqual(['1234'])
+    })
+    it('should split variables', () => {
+        expect(calc.splitElement('xyz')).toStrictEqual(['x', '*', 'y', '*', 'z'])
+    })
+    it('should split variables, but leave digits', () => {
+        expect(calc.splitElement('12x34')).toStrictEqual(['12', '*', 'x', '*', '34'])
+    })
+    it('should split variables, but leave digits, multiple variables', () => {
+        expect(calc.splitElement('12xy34')).toStrictEqual(['12', '*', 'x', '*', 'y', '*', '34'])
+    })
+})
+
+describe('#subArray', () => {
+    it('should return parts of an array', () => {
+        const inputArray = ['a', 'b', 'c', 'd', 'e', 'f']
+        const ixStart = 2
+        const ixEnd = 4
+        const resultArray = ['c', 'd', 'e']
+        expect(calc.subArray(inputArray, 2, 4)).toStrictEqual(resultArray)
+    })
+})
+
+describe('#extractParethesis', () => {
+    it('should extract the content of the first parenthesis', () => {
+        expect(calc.extractParenthesis(['(', '2', 'y', ')']))
+            .toStrictEqual({
+                subFormula: ['2', 'y'],
+                ixStart: 0,
+                ixEnd: 3
+            })
+    })
+    it('should throw if end of paranthesis cannot be found', () => {
+        expect(() => calc.extractParenthesis(['(', '2', 'y'])).toThrow('End of paranthesis not found!')
+    })
+    it('should extract the content of the first parenthesis further in', () => {
+        expect(calc.extractParenthesis(['x', '(', '2', 'y', ')']))
+            .toStrictEqual({
+                subFormula: ['2', 'y'],
+                ixStart: 1,
+                ixEnd: 4
+            })
+    })
+    it('should extract the content of the innermost parenthesis', () => {
+        expect(calc.extractParenthesis(['x', '(', '(', 'y', ')', ')']))
+            .toStrictEqual({
+                subFormula: ['y'],
+                ixStart: 2,
+                ixEnd: 4
+            })
+    })
+    it('should extract the content of the innermost, innermost parenthesis', () => {
+        expect(calc.extractParenthesis(['x', '(', '(', 'y', 'x', '(', '123', 'y', ')', ')']))
+            .toStrictEqual({
+                subFormula: ['123','y'],
+                ixStart: 5,
+                ixEnd: 8
+            })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,96 @@
+const ops = ['^', '*', '/', '+', '-']
+
+const splitForumla = (formula: string): Array<string> => {
+    let regexStr = `(${ops.map((op) => '\\' + op).join('|')})`
+    const regex = new RegExp(regexStr);
+    return formula.replace(/ /g, '').split(regex)
+};
+
+export const splitElement = (element: string) => {
+    const result = []
+    let digitMemory = ''
+    for (let i = 0; i < element.length; i++) {
+        const char = parseInt(element[i])
+        if (isNaN(char)) {
+            if (digitMemory.length) {
+                result.push(digitMemory)
+                result.push('*')
+                digitMemory = ''
+            }
+            result.push(element[i])
+            result.push('*')
+        } else {
+            digitMemory += char
+        }
+    }
+    if (digitMemory) {
+        result.push(digitMemory)
+    } else {
+        result.splice(-1, 1)
+    }
+    return result
+}
+
+export const addImplicitMultiply = (formulaArray: string[]) => {
+    let ix = 0;
+    for (const element of formulaArray) {
+        if (element.length > 1) {
+            const elements: string[] = splitElement(element)
+            if (elements.length > 0) {
+                formulaArray.splice(ix, 1, ...elements)
+            }
+        }
+        ix++
+    }
+};
+
+type extractedParenthesis = {
+    subFormula: string[],
+    ixStart: number,
+    ixEnd: number
+}
+
+export const subArray = (array: string[], ixStart: number, ixEnd: number): string[] => {
+    const result = []
+    array.forEach((elem, ix) => {
+        if (ix > ixStart && ix < ixEnd) {
+            result.push(elem)
+        }
+    })
+    return result
+}
+
+export function extractParenthesis(formulaArray: string[], startSearch: number = 0): extractedParenthesis  {
+    const firstParanStart = formulaArray.indexOf('(', startSearch)
+    const firstParanEnd = formulaArray.indexOf(')', firstParanStart)
+    if (firstParanEnd === -1) {
+        throw new Error('End of paranthesis not found!')
+    }
+    const secondParanStart = formulaArray.indexOf('(', firstParanStart + 1);
+    if (secondParanStart > 0 && secondParanStart < firstParanEnd) {
+        return extractParenthesis(formulaArray, secondParanStart)
+    }
+
+    const subFormula = subArray(formulaArray, firstParanStart, firstParanEnd)
+    return {
+        subFormula,
+        ixStart: firstParanStart,
+        ixEnd: firstParanEnd
+    }
+}
+
+const resolve = (x: number, formulaArray: string[]) => {
+    while(formulaArray.includes('(')) {
+        const {subFormula, ixStart, ixEnd} = extractParenthesis(formulaArray)
+    }
+};
 
 export default (x: number, formula: string): number => {
+    const formulaArray: string[] = splitForumla(formula);
+    addImplicitMultiply(formulaArray)
+    resolve(x, formulaArray)
+
+    console.log('regexStr:' + JSON.stringify(formulaArray), {...formulaArray})
+
     return 1
 }
